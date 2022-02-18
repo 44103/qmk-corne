5593414bb3aee986ef2e72a7294d1c9bb80467c5
kenko <universeengineer@gmail.com>
2022-02-19 08:48:52 +0900

    :sparkles: make base keymap

---
+++ qmk_firmware/keyboards/crkbd/keymaps/base/config.h
@@ -0,0 +1,37 @@
+#pragma once
+
+//#define USE_MATRIX_I2C
+
+/* Select hand configuration */
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+//#define TAPPING_FORCE_HOLD
+//#define TAPPING_TERM 100
+
+#ifdef RGBLIGHT_ENABLE
+    #define RGBLIGHT_EFFECT_BREATHING
+    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+    #define RGBLIGHT_EFFECT_SNAKE
+    #define RGBLIGHT_EFFECT_KNIGHT
+    #define RGBLIGHT_EFFECT_CHRISTMAS
+    #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+    #define RGBLIGHT_EFFECT_RGB_TEST
+    #define RGBLIGHT_EFFECT_ALTERNATING
+    #define RGBLIGHT_EFFECT_TWINKLE
+    #define RGBLIGHT_LIMIT_VAL 120
+    #define RGBLIGHT_HUE_STEP 10
+    #define RGBLIGHT_SAT_STEP 17
+    #define RGBLIGHT_VAL_STEP 17
+#endif
+
+#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
+
+#define LCA_DEL LCA(KC_DEL)
+#define LCA_PAUS LCA(KC_PAUS)
+
+#define LT_MO_1 LT(MO(1), KC_LANG2)
+#define LT_MO_2 LT(MO(2), KC_LANG1)
--- readme.md
+++ readme.md
@@ -1,4 +1,6 @@
 
 ```
 git clone --recurse-submodules https://github.com/qmk/qmk_firmware.git
+docker-compose run --rm compile qmk new-keymap
+docker-compose run --rm compile
 ```
